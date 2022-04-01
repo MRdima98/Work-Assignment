49b45cb716fe738a8e50920209c2589ab480cc5b
MRdima98 <62344480+MRdima98@users.noreply.github.com>
2022-04-01 08:30:40 +0200

    more readable

--- articoli.py
+++ articoli.py
@@ -7,6 +7,7 @@ Prevedere la possibilità di passare un argomento al comando.
 Se questo parametro è presente, salvare sul file txt solo le notizie
 (tra le 5 analizzate) che contengano quella stringa
 '''
+from distutils.filelist import findall
 from pickletools import string1
 import urllib.request    
 import re
@@ -14,7 +15,7 @@ from wsgiref.util import request_uri
 
 # scarica su fil l'html del sito
 urllib.request.urlretrieve("https://www.ansa.it/", "ansa.txt")
-
+#urllib.request.urlretrieve("https://www.ansa.it/", "bug.txt")
 
 # cerca tutti i titoli
 with open('ansa.txt', encoding='utf-8', errors='ignore') as f:
@@ -28,17 +29,25 @@ open('ansa.txt','w').close()
 def firstFiveNews(result,s):
     f = open("ansa.txt",'a',encoding='utf-8')
     for i in range(0,5):
-        tmp=re.findall('.html">(.*)',result[i])[0]
-        if(s in tmp):
-            if('<br>' in tmp):
-                f.write(re.findall('.html">(.*)<br>',result[i])[0])
-                f.write('. ')
-                f.write(re.findall('<br>(.*)',result[i])[0])
-                f.write('\n')
-            else:
-                f.write(tmp)
-                f.write('\n')
+        cleanS=cleanString(result[i],i)
+        if(s in cleanS):
+            f.write(cleanS)
     f.close()
+
+def cleanString(s,i):
+    '''
+    Funzione che filtra il link e formatta correttamente il ritorno a capo
+    '''
+    s=re.findall('.html">(.*)',s)[0]
+    if('<br>' in s):
+        s1=re.findall('(.*)<br>',s)[0]
+        s2=re.findall('<br>(.*)',s)[0]
+        s=s1+'\n'+s2
+    if('<br />' in s):
+        s1=re.findall('(.*)<br />',s)[0]
+        s2=re.findall('<br />(,*)',s)[0]
+        s=s1+'\n'+s2 
+    return 'Notizia '+str(i+1)+':\n'+s+'\n\n'
 
 if __name__ == "__main__":
     firstFiveNews(result,input("Digitare la stringa da cercare nei primi 5 ariticoli o premere enter: "))
